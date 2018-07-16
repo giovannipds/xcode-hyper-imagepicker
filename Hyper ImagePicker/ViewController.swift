d37ed3b5c926b974b229232b39bf528afa75d9f9
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-07-16 13:11:58 -0300

    Implement hyper image picker

--- Hyper ImagePicker/ViewController.swift
+++ Hyper ImagePicker/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import ImagePicker
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ImagePickerDelegate {
+    
+    let imagePickerController = ImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePickerController.delegate = self
+        imagePickerController.imageLimit = 1
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func pickImage(_ sender: UIButton) {
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func wrapperDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        print("wrapper did press")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        print("done did press")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func cancelButtonDidPress(_ imagePicker: ImagePickerController) {
+        print("cancel pressed")
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
---
+++ Pods/Target Support Files/Pods-Hyper ImagePicker/Pods-Hyper ImagePicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hyper_ImagePicker : NSObject
+@end
+@implementation PodsDummy_Pods_Hyper_ImagePicker
+@end
